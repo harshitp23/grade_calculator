bc384f5699045f48c3d6dfea82ac889a9c97217b
Harshit <harshitprithiani@gmail.com>
2026-02-01 17:12:26 -0600

    Add method to find subject by user and subject code

--- gradecalculator/src/main/java/com/harshit/gradecalculator/repository/SubjectRepository.java
+++ gradecalculator/src/main/java/com/harshit/gradecalculator/repository/SubjectRepository.java
@@ -4,10 +4,13 @@ import com.harshit.gradecalculator.model.Subject;
 import com.harshit.gradecalculator.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List; // <--- Don't forget this import!
+import java.util.Optional;
 
 public interface SubjectRepository extends JpaRepository<Subject, Integer> {
     
     // 👇 ADD THIS LINE 👇
     List<Subject> findByUser(User user);
+
+    Optional<Subject> findByUserAndSubjectCode(User user, String subjectCode);
 
 }
