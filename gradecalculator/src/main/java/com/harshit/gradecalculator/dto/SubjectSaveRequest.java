01e2f87faa240177c4459892d01a32e0062acfdb
Harshit <harshitprithiani@gmail.com>
2026-02-10 01:02:09 -0600

    Refactor SubjectSaveRequest to simplify structure
    Removed unnecessary fields and updated component list type.

--- gradecalculator/src/main/java/com/harshit/gradecalculator/dto/SubjectSaveRequest.java
+++ gradecalculator/src/main/java/com/harshit/gradecalculator/dto/SubjectSaveRequest.java
@@ -4,31 +4,38 @@ import java.util.List;
 
 public class SubjectSaveRequest {
     private Integer id;
-    private String subjectName;
-    private String subjectCode;
-    private Integer credits;
-    private String status;
     private Boolean useTotalPoints;
-    private List<ComponentSaveRequest> components;
+    private List<ComponentPayload> components;
 
     public Integer getId() { return id; }
     public void setId(Integer id) { this.id = id; }
 
-    public String getSubjectName() { return subjectName; }
-    public void setSubjectName(String subjectName) { this.subjectName = subjectName; }
-
-    public String getSubjectCode() { return subjectCode; }
-    public void setSubjectCode(String subjectCode) { this.subjectCode = subjectCode; }
-
-    public Integer getCredits() { return credits; }
-    public void setCredits(Integer credits) { this.credits = credits; }
-
-    public String getStatus() { return status; }
-    public void setStatus(String status) { this.status = status; }
-
     public Boolean getUseTotalPoints() { return useTotalPoints; }
     public void setUseTotalPoints(Boolean useTotalPoints) { this.useTotalPoints = useTotalPoints; }
 
-    public List<ComponentSaveRequest> getComponents() { return components; }
-    public void setComponents(List<ComponentSaveRequest> components) { this.components = components; }
+    public List<ComponentPayload> getComponents() { return components; }
+    public void setComponents(List<ComponentPayload> components) { this.components = components; }
+
+    public static class ComponentPayload {
+        private Integer id;
+        private String name;
+        private Double weight;
+        private Double score;
+        private Double totalPoints;
+
+        public Integer getId() { return id; }
+        public void setId(Integer id) { this.id = id; }
+
+        public String getName() { return name; }
+        public void setName(String name) { this.name = name; }
+
+        public Double getWeight() { return weight; }
+        public void setWeight(Double weight) { this.weight = weight; }
+
+        public Double getScore() { return score; }
+        public void setScore(Double score) { this.score = score; }
+
+        public Double getTotalPoints() { return totalPoints; }
+        public void setTotalPoints(Double totalPoints) { this.totalPoints = totalPoints; }
+    }
 }
