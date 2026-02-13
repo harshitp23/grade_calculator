f0887f74339f865b6ac145f0d67548c9210570cf
Harshit <harshitprithiani@gmail.com>
2026-02-12 23:27:28 -0600

    Update SubjectSaveRequest.java

--- gradecalculator/src/main/java/com/harshit/gradecalculator/dto/SubjectSaveRequest.java
+++ gradecalculator/src/main/java/com/harshit/gradecalculator/dto/SubjectSaveRequest.java
@@ -5,7 +5,9 @@ import java.util.List;
 public class SubjectSaveRequest {
     private Integer id;
     private Boolean useTotalPoints;
-    private List<ComponentPayload> components;
+    
+    // 👇 CHANGED: Use the standalone ComponentSaveRequest class
+    private List<ComponentSaveRequest> components; 
 
     public Integer getId() { return id; }
     public void setId(Integer id) { this.id = id; }
@@ -13,29 +15,6 @@ public class SubjectSaveRequest {
     public Boolean getUseTotalPoints() { return useTotalPoints; }
     public void setUseTotalPoints(Boolean useTotalPoints) { this.useTotalPoints = useTotalPoints; }
 
-    public List<ComponentPayload> getComponents() { return components; }
-    public void setComponents(List<ComponentPayload> components) { this.components = components; }
-
-    public static class ComponentPayload {
-        private Integer id;
-        private String name;
-        private Double weight;
-        private Double score;
-        private Double totalPoints;
-
-        public Integer getId() { return id; }
-        public void setId(Integer id) { this.id = id; }
-
-        public String getName() { return name; }
-        public void setName(String name) { this.name = name; }
-
-        public Double getWeight() { return weight; }
-        public void setWeight(Double weight) { this.weight = weight; }
-
-        public Double getScore() { return score; }
-        public void setScore(Double score) { this.score = score; }
-
-        public Double getTotalPoints() { return totalPoints; }
-        public void setTotalPoints(Double totalPoints) { this.totalPoints = totalPoints; }
-    }
+    public List<ComponentSaveRequest> getComponents() { return components; }
+    public void setComponents(List<ComponentSaveRequest> components) { this.components = components; }
 }
