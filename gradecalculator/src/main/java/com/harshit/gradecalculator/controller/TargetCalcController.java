f40a948725be620a16129f4ca34c034741f6ed02
Harshit <harshitprithiani@gmail.com>
2026-02-13 16:10:58 -0600

    Create TargetCalcController.java

---
+++ gradecalculator/src/main/java/com/harshit/gradecalculator/controller/TargetCalcController.java
@@ -0,0 +1,29 @@
+package com.harshit.gradecalculator.controller;
+
+import com.harshit.gradecalculator.model.Subject;
+import com.harshit.gradecalculator.model.User;
+import com.harshit.gradecalculator.repository.SubjectRepository;
+import com.harshit.gradecalculator.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class TargetCalcController {
+
+    @Autowired private UserRepository userRepository;
+    @Autowired private SubjectRepository subjectRepository;
+
+    @GetMapping("/target-calculator")
+    public String showTargetPage(Model model, @AuthenticationPrincipal UserDetails userDetails) {
+        User user = userRepository.findByUsername(userDetails.getUsername()).orElseThrow();
+        List<Subject> subjects = subjectRepository.findByUser(user);
+        model.addAttribute("subjects", subjects);
+        return "target-calculator";
+    }
+}
