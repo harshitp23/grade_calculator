d6ceee7574a253cb8799cde306c5361a6f4a043e
Harshit <harshitprithiani@gmail.com>
2026-02-13 16:57:36 -0600

    Update SubjectRepository.java

--- gradecalculator/src/main/java/com/harshit/gradecalculator/repository/SubjectRepository.java
+++ gradecalculator/src/main/java/com/harshit/gradecalculator/repository/SubjectRepository.java
@@ -3,15 +3,17 @@ package com.harshit.gradecalculator.repository;
 import com.harshit.gradecalculator.model.Subject;
 import com.harshit.gradecalculator.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List; // <--- Don't forget this import!
+import org.springframework.stereotype.Repository;
+import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface SubjectRepository extends JpaRepository<Subject, Integer> {
-    
-    // 👇 ADD THIS LINE 
-    List<Subject> findByUserAndStatus(User user, String status);
+    // 👇 Fixes SimulatorController and SubjectController build errors
+    List<Subject> findByUser(User user); 
+
+    // 👇 Fixes TargetCalcController build error
+    List<Subject> findByUserAndStatus(User user, String status); 
 
     Optional<Subject> findByUserAndSubjectCode(User user, String subjectCode);
-
 }
-
