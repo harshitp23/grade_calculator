31e35ca301277e2b8506075e5a4de9c2e21ebc22
Harshit <harshitprithiani@gmail.com>
2026-02-13 01:20:57 -0600

    Update CanvasSyncController.java

--- gradecalculator/src/main/java/com/harshit/gradecalculator/controller/CanvasSyncController.java
+++ gradecalculator/src/main/java/com/harshit/gradecalculator/controller/CanvasSyncController.java
@@ -53,10 +53,10 @@ public class CanvasSyncController {
             }
             subjectsUpdated++;
 
-            // 2. Clear old components to perform a fresh sync
+            // 2. Clear old components
             componentRepository.deleteBySubject(subject);
 
-            // 3. Add New Components (Groups) & Assignments
+            // 3. Add New Components & Assignments
             if (coursePayload.getComponents() != null) {
                 for (CanvasComponentPayload compPayload : coursePayload.getComponents()) {
                     
@@ -66,23 +66,21 @@ public class CanvasSyncController {
                     comp.setWeight(compPayload.getWeight());
                     comp.setScore(compPayload.getScore());
                     comp.setTotalPoints(compPayload.getTotalPoints());
+                    comp.setDropLowest(compPayload.getDropLowest()); // Save drop rule
                     
-                    // Save component first to get ID
                     comp = componentRepository.save(comp);
                     componentsUpdated++;
 
-                    // Add assignments if present
                     if (compPayload.getAssignments() != null) {
                         for (CanvasAssignmentPayload assignPayload : compPayload.getAssignments()) {
                             Assignment assign = new Assignment();
-                            assign.setComponent(comp); // Link to parent
+                            assign.setComponent(comp);
                             assign.setName(assignPayload.getName());
                             assign.setScore(assignPayload.getScore());
                             assign.setTotalPoints(assignPayload.getTotalPoints());
                             
                             comp.getAssignments().add(assign);
                         }
-                        // Save again to cascade assignments
                         componentRepository.save(comp);
                     }
                 }
